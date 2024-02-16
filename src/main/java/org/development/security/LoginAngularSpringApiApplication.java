ba287db9be63c613f3a1c1396258d8a57282e12e
Gabriel Alejandro Pina <gabrielpinia19@hotmail.es>
2024-02-16 01:51:50 -0400

    init project

---
+++ src/main/java/org/development/security/LoginAngularSpringApiApplication.java
@@ -0,0 +1,13 @@
+package org.development.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginAngularSpringApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginAngularSpringApiApplication.class, args);
+    }
+
+}
